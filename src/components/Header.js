6548b660e3675394f8b69893b4a2a9eaac70f694
David Gagnon <toogreen@15inch.lan>
2020-08-14 01:25:34 -0400

    realigned logo at the top and text as it was too much on the left

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,7 @@ function Header() {
 				src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
 				alt="Problem?" 
 			/>
-
-		 	<p>Meme Generator</p>
+	 		<p>Meme Generator</p>
 		</header>
 	)
 }
